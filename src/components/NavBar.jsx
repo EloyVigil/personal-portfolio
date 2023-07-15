5af09baff20b114b4162aaab97d57b620d0a63a9
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-07-14 21:50:57 -0700

    Images and Styling CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,8 @@
-import { Navbar } from "bootstrap";
 import { useState, useEffect } from "react";
+import logo from '../views/img/logo.svg';
+import navIcon1 from '../views/img/nav-icon1.svg';
+import navIcon2 from '../views/img/nav-icon2.svg';
+import navIcon3 from '../views/img/nav-icon3.svg';
 
 export const NavBar = () => {
 
@@ -28,7 +31,7 @@ export const NavBar = () => {
         <nav class="navbar navbar-expand-lg bg-body-tertiary" className={scrolled ? "scrolled" : ""} >
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
-                    <img src={''} alt="My Logo" />
+                    <img src={logo} alt="My Logo" />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -39,9 +42,9 @@ export const NavBar = () => {
                     <a href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} > Projects</a>
                     <span className="navbar-text" >
                         <div className="social-icon" >
-                            <a href="#"><img src={''} alt="Icons" /></a>
-                            <a href="#"><img src={''} alt="Icons" /></a>
-                            <a href="#"><img src={''} alt="Icons" /></a>
+                            <a href="#"><img src={navIcon1} alt="Icon1" /></a>
+                            <a href="#"><img src={navIcon2} alt="Icon2" /></a>
+                            <a href="#"><img src={navIcon3} alt="Icon3" /></a>
                         </div>
                         <button className="vvd" onClick={() => console.log('Connect')}><span>Let's Connect</span></button>
                     </span>
