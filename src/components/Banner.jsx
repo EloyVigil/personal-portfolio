62c4efcd5d526fbb6714a1cc6d9c25629a047e15
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-07-20 18:02:49 -0700

    Banner Component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,61 @@
+import { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerImg from '../views/img/header-img.svg';
+
 export const Banner = () => {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Software Developer", "Web Developer", "Full Stack Developer"];
+    const [text, setText] = useState("");
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === "") {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+
+    }
+
     return (
-        <>
-            
-        </>
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className='tagline'>Welcome to the best portfolio</span>
+                        <h1>{`Hi I'm Eloy a `}<span className='wrap'>{ text }</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestias, quae dolorem, culpa totam minima aliquid distinctio, debitis quos accusantium consequuntur eaque impedit omnis dolorum at autem. Vel, facilis quam.</p>
+                        <button onClick={() => console.log('Connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt='Space Man' />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
