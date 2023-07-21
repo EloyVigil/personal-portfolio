6e8c2743b236968e69328ce80076a129d517d8c3
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-07-20 18:03:19 -0700

    Banner Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
