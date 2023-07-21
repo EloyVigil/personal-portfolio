5ed5a186a5dc3c330a11122b0b75ba0d5149a3f7
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-07-20 18:24:44 -0700

    Nav Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../views/img/logo.svg';
 import navIcon1 from '../views/img/nav-icon1.svg';
 import navIcon2 from '../views/img/nav-icon2.svg';
@@ -11,7 +12,7 @@ export const NavBar = () => {
 
     useEffect(() => {
         const onScroll = () => {
-            if(window.scrollY > 50){
+            if (window.scrollY > 50) {
                 setScrolled(true);
             } else {
                 setScrolled(false);
@@ -28,28 +29,29 @@ export const NavBar = () => {
     }
 
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary" className={scrolled ? "scrolled" : ""} >
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src={logo} alt="My Logo" />
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <a href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')} >Home</a> ||
-                    <a href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} > Skills</a> ||
-                    <a href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} > Projects</a>
-                    <span className="navbar-text" >
-                        <div className="social-icon" >
-                            <a href="#"><img src={navIcon1} alt="Icon1" /></a>
-                            <a href="#"><img src={navIcon2} alt="Icon2" /></a>
-                            <a href="#"><img src={navIcon3} alt="Icon3" /></a>
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src={ logo } alt="Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="#"><img src={ navIcon1 } alt="" /></a>
+                            <a href="#"><img src={ navIcon2 } alt="" /></a>
+                            <a href="#"><img src={ navIcon3 } alt="" /></a>
                         </div>
-                        <button className="vvd" onClick={() => console.log('Connect')}><span>Let's Connect</span></button>
                     </span>
-                </div>
-            </div>
-        </nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
