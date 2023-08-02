3698856a1689c56977419d20af8e9230cbe7c97b
Eloy Vigil <112999750+EloyVigil@users.noreply.github.com>
2023-08-01 17:05:31 -0700

    Final Touches

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,13 +3,27 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import TrackVisibility from 'react-on-screen';
 import 'animate.css';
-import java from '../views/img/java-vertical.svg';
+import java from '../views/img/java.svg';
 import javascript from '../views/img/javascript.svg';
 import python from '../views/img/python.svg';
 import mysql from '../views/img/mysql.svg';
 import mongodb from '../views/img/mongodb.svg';
 import flask from '../views/img/flask.svg';
-
+import ajax from '../views/img/ajax.png';
+import bootstrap from '../views/img/bootstrap.svg';
+import css from '../views/img/css.svg';
+import express from '../views/img/express.svg';
+import github from '../views/img/github.svg';
+import html from '../views/img/html.svg';
+import jquery from '../views/img/jquery.svg';
+import json from '../views/img/json.svg';
+import jsp from '../views/img/jsp.png';
+import jstl from '../views/img/jstl.jpg';
+import materialui from '../views/img/materialui.svg';
+import postman from '../views/img/postman.svg';
+import react from '../views/img/react.svg';
+import node from '../views/img/nodejs.svg';
+import springimg from '../views/img/spring.svg';
 import colorSharp from '../views/img/color-sharp.png';
 
 export const Skills = () => {
@@ -48,6 +62,14 @@ export const Skills = () => {
                             </TrackVisibility>
                             <Carousel responsive={responsive} infinite={true} className='skill-slider'>
                                 <div className='item'>
+                                    <img src={html} alt="HTML 5" />
+                                    <h5>HTML 5</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={css} alt="CSS 3" />
+                                    <h5>CSS 3</h5>
+                                </div>
+                                <div className='item'>
                                     <img src={java} alt="Java" />
                                     <h5>Java 8</h5>
                                 </div>
@@ -60,6 +82,10 @@ export const Skills = () => {
                                     <h5>Python 3</h5>
                                 </div>
                                 <div className='item'>
+                                    <img src={react} alt="React" />
+                                    <h5>React</h5>
+                                </div>
+                                <div className='item'>
                                     <img src={mysql} alt="MySQL" />
                                     <h5>MySQL DB</h5>
                                 </div>
@@ -68,36 +94,56 @@ export const Skills = () => {
                                     <h5>MongoDB</h5>
                                 </div>
                                 <div className='item'>
+                                    <img src={springimg} alt="Spring" />
+                                    <h5>Spring Framework</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={node} alt="Node.js" />
+                                    <h5>Node.js</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={express} alt="Express.js" />
+                                    <h5>Express</h5>
+                                </div>
+                                <div className='item'>
                                     <img src={flask} alt="Flask" />
-                                    <h5>Python 3</h5>
+                                    <h5>Flask</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={ajax} alt="AJAX" />
+                                    <h5>AJAX<br/>Asynchronous Javascript and XML</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={json} alt="JSON" />
+                                    <h5>JSON</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={jsp} alt="JSP" />
+                                    <h5>JSP<br/>Java Server Pages</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={jquery} alt="JQuery" />
+                                    <h5>JQuery</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={jstl} alt="JSTL" />
+                                    <h5>JSTL<br/>Java Server Tag Library</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={postman} alt="Postman" />
+                                    <h5>Postman</h5>
                                 </div>
                                 <div className='item'>
-                                    <img src={python} alt="Python" />
-                                    <h5>Python 3</h5>
+                                    <img src={github} alt="GitHub" />
+                                    <h5>Git Hub</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={bootstrap} alt="Bootstrap" />
+                                    <h5>Bootstrap UI</h5>
+                                </div>
+                                <div className='item'>
+                                    <img src={materialui} alt="Material UI" />
+                                    <h5>Material UI</h5>
                                 </div>
                             </Carousel>
                         </div>
